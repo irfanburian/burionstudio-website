326b89c3b833082bb3a641420db7667b828d4055
irfanburian <irfanburian@gmail.com>
2026-08-31 22:17:11 +0300

    Keep carousel behavior clean after stylesheet integration

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-(()=>{const link=document.createElement("link");link.rel="stylesheet";link.href="css/selected-work-carousel-fix.css?v=20260831-1";document.head.appendChild(link)})();
-
 document.addEventListener("DOMContentLoaded",()=>{
   const q=s=>document.querySelector(s), menu=q("#mobileMenu"), menuBtn=q(".menu-button"), header=q(".site-header");
   document.documentElement.lang="en";
